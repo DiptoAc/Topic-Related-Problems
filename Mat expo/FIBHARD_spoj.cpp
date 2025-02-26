707ed78a4fed7cfe40ac70ac178fd661eaff8e72
Sudipto Acharjee <86905373+DiptoAc@users.noreply.github.com>
2025-02-27 00:56:16 +0600

    Create FIBHARD_spoj.cpp
    This problem extends the scope of thinking. 
    You need another code the bruteforce the mod value.

---
+++ Mat expo/FIBHARD_spoj.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define int long long
+#define fast ios_base::sync_with_stdio();cin.tie(NULL);cout.tie(NULL);
+ll mod=998244353;
+ll M=1996488708;
+ll mx =1e18+1;
+struct Mat {
+  int n, m;
+  vector<vector<int>> a;
+  Mat() { }
+  Mat(int _n, int _m) {n = _n; m = _m; a.assign(n, vector<int>(m, 0)); }
+  Mat(vector< vector<int> > v) { n = v.size(); m = n ? v[0].size() : 0; a = v; }
+  inline void make_unit() {
+    assert(n == m);
+    for (int i = 0; i < n; i++)  {
+      for (int j = 0; j < n; j++) a[i][j] = i == j;
+    }
+  }
+  inline Mat operator + (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] + b.a[i][j]) % mod;
+      }
+    }
+    return ans;
+  } 
+  inline Mat operator - (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] - b.a[i][j] + mod) % mod;
+      }
+    }
+    return ans;
+  }
+  inline Mat operator * (const Mat &b) {
+    assert(m == b.n);
+    Mat ans = Mat(n, b.m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < b.m; j++) {
+        for(int k = 0; k < m; k++) {
+          ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % mod) % mod;
+        }
+      }
+    }
+    return ans;
+  }
+  inline Mat pow(long long k) {
+    assert(n == m);
+    Mat ans(n, n), t = a; ans.make_unit();
+    while (k) {
+      if (k & 1) ans = ans * t;
+      t = t * t;
+      k >>= 1;
+    }
+    return ans;
+  }
+  inline Mat& operator += (const Mat& b) { return *this = (*this) + b; }
+  inline Mat& operator -= (const Mat& b) { return *this = (*this) - b; }
+  inline Mat& operator *= (const Mat& b) { return *this = (*this) * b; }
+  inline bool operator == (const Mat& b) { return a == b.a; }
+  inline bool operator != (const Mat& b) { return a != b.a; }
+};
+int32_t main()
+{
+    fast
+    ll n,m,i,j=1,k,p,q,tc=1,cs=0;cin>>tc;
+    while(tc--)
+    {
+        string s;
+        cin>>s;
+
+        ll num=0;
+        for(auto it:s)
+        {
+            num*=10;
+            num=(__int128)(num+(it-'0'))%M;
+        }
+        vector<vector<ll>>v=
+        {
+            {1,1},
+            {1,0}
+        },
+        u=
+        {
+            {1},
+            {0}
+        };
+        Mat a(v);
+        Mat b(u);
+        Mat ans=a.pow(num);
+        ans=ans*b;
+        cout<<ans.a[1][0]<<endl;
+    }
+}
+
+/*
+Problem link:https://www.spoj.com/problems/FIBHARD/
+This problem requires two part. Brute forcing to find the number that must be used to mod the string with .
+And then matrix exponetiation part.
+*/
